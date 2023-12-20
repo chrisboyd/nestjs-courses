f23afc2c6353b8beb35faf52692de6fdf2a1f015
Chris Boyd <christopher.boyd@withyouwithme.com>
2023-12-20 14:51:31 -0800

    cqrs part 2

--- advanced-architecture/src/alarms/application/commands/create-alarm.command-handler.ts
+++ advanced-architecture/src/alarms/application/commands/create-alarm.command-handler.ts
@@ -1,8 +1,9 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { CreateAlarmCommand } from './create-alarm.command';
 import { Logger } from '@nestjs/common';
 import { AlarmFactory } from '../../domain/factories/alarm.factory';
 import { AlarmRepository } from '../ports/alarm.repository';
+import { AlarmCreatedEvent } from '../../domain/events/alarm-created.event';
 
 @CommandHandler(CreateAlarmCommand)
 export class CreateAlarmCommandHandler
@@ -13,6 +14,7 @@ export class CreateAlarmCommandHandler
   constructor(
     private readonly alarmRepository: AlarmRepository,
     private readonly alarmFactory: AlarmFactory,
+    private readonly eventBus: EventBus,
   ) {}
 
   async execute(command: CreateAlarmCommand) {
@@ -20,6 +22,8 @@ export class CreateAlarmCommandHandler
       `Processing "CreateAlarmCommand": ${JSON.stringify(command)}`,
     );
     const alarm = this.alarmFactory.create(command.name, command.severity);
+    this.eventBus.publish(new AlarmCreatedEvent(alarm));
+
     return this.alarmRepository.save(alarm);
   }
 }
