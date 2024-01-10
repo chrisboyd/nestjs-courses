ec061e5063fbaab0281475d52acbe5988c223d32
Chris Boyd <christopher.boyd@withyouwithme.com>
2024-01-10 15:16:58 -0800

    accessing IoC module

--- advanced-concepts/src/app.module.ts
+++ advanced-concepts/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { RewardsService } from './rewards/rewards.service';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { CronModule } from './cron/cron.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, SchedulerModule, CronModule],
   controllers: [AppController],
   providers: [AppService, RewardsService],
 })
